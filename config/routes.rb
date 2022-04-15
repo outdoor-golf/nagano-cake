120482289c9a3abf4da5dac6b3f5a0865b5e0acf
Deby2404 <dedede2404@gmail.com>
2022-04-15 12:49:24 +0000

    order controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+
   namespace :admin do
-    get 'homes/top'
+
   end
-  get 'homes/top'
-  get 'home/about' => 'homes#about', as: 'about'
+
   devise_for :admins
   devise_for :customers
   resources:foods,only:[:new,:create,:index]
+  resources:orders,only:[:index,:show,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
