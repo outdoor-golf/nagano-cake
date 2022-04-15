8e18df5d38f28fb87e36c77a1950ae85cd6a90b2
hassy1220 <hashihassy.1220.gmail.com>
2022-04-15 09:37:38 +0000

    food#create

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,5 +5,15 @@ class FoodsController < ApplicationController
 
   end
   def create
+    food = Food.new(food_params)
+    food.save
+    redirect_to new_food_path
+  end
+  def index
+    @foods = Food.all
+  end
+  private
+  def food_params
+    params.require(:food).permit(:name,:explanation,:genre_id,:price,:sale_status)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
-  resources:foods,only:[:new,:create]
+  resources:foods,only:[:new,:create,:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
