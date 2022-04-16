9e0b860629823b07ee010ed0f089a616e13f61d0
Deby2404 <dedede2404@gmail.com>
2022-04-16 07:37:12 +0000

    add layouts to orders/index,show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/show'
   namespace :admin do
     get 'homes/top'
   end
@@ -8,5 +10,6 @@ Rails.application.routes.draw do
   devise_for :customers
   resources:foods,only:[:new,:create,:index,:show,:edit,:update]
   resources:genres,only:[:new,:create]
+  resources:orders,only:[:index,:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
