e69bd762e99bde2400850eefa89137dc8779dab8
Deby2404 <dedede2404@gmail.com>
2022-04-16 10:39:26 +0000

    add routes and controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,21 +1,8 @@
 class OrdersController < ApplicationController
-  
-  
-  def new
-  end
-
-  def check
-  end
-
-  def create
-  end
-
   def index
+    
   end
 
   def show
-  end
-
-  def complete
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'orders/edit'
+  get 'orders/destroy'
+  get 'orders/update'
   get 'orders/index'
   get 'orders/show'
   namespace :admin do
@@ -14,5 +17,11 @@ Rails.application.routes.draw do
   devise_for :customers
   resources:foods,only:[:new,:create,:index,:show,:edit,:update]
   resources:genres,only:[:new,:create]
+  resources:orders,except:[:edit,:destroy,:update] do
+   collection do
+    get "complete"
+    post "check"
+   end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
