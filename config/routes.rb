d85e7216154c490c4f0dab241d601e7a8b560676
hassy1220 <hashihassy.1220.gmail.com>
2022-04-16 07:51:04 +0000

    food

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   namespace :admin do
+    resources:foods,only:[:new,:create,:index,:show,:edit,:update]
+    resources:genres,only:[:new,:create]
     get 'homes/top'
   end
+  resources:foods,only:[:index]
   get 'homes/top'
   get 'home/about' => 'homes#about', as: 'about'
   devise_for :admins
   devise_for :customers
-  resources:foods,only:[:new,:create,:index,:show,:edit,:update]
-  resources:genres,only:[:new,:create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
