0eab03f999461cc257373b393e807e7b5ae795ad
Deby2404 <dedede2404@gmail.com>
2022-04-16 08:25:36 +0000

    ルーティングのところを削除

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,6 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about', as: 'about'
   devise_for :admins
   devise_for :customers
-  resources:foods,only:[:new,:create,:index,:show,:edit,:update]
-  resources:genres,only:[:new,:create]
   resources:orders,only:[:index,:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
