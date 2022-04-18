57b80d5bce0163bc8470c6c1bdc69dd20e6fcf62
EC2 Default User <sqt01v@gmail.com>
2022-04-18 04:22:30 +0000

    ログインログアウト

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 class Admin::RegistrationsController < Devise::RegistrationsController
-  
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    admin_homes_top_path
+  end
+
+
+
+  def after_sign_out_path_for(resource)
+    admin_homes_top_path
+  end
+
+
   # before_action :configure_permitted_parameters, if: :devise_controller?
 
   # protected
